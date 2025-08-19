5c9734222e98c37db41ee411d19610b58505e552
Srishti <141471628+Srishti1213@users.noreply.github.com>
2025-08-19 13:03:04 +0530

    Add files via upload

---
+++ temparature and humidity detecton.cpp
@@ -0,0 +1,63 @@
+#include <WiFi.h> 
+#include <ThingESP.h>
+#include <DHT.h>
+
+#define DHTPIN 5 
+#define DHTTYPE DHT11     // DHT 11
+DHT dht(DHTPIN, DHTTYPE);
+
+ThingESP32 thing("Username", "Project name", "Username");
+
+unsigned long previousMillis = 0;
+const long INTERVAL = 6000;  
+
+void setup()
+{
+  Serial.begin(115200);
+
+ // pinMode(LED, OUTPUT);
+
+  thing.SetWiFi("ABHIJIT_2.4G", "Year@2025");
+
+  thing.initDevice();
+
+}
+
+
+String HandleResponse(String query)
+{
+
+ float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+  Serial.println("Temperature :");
+  Serial.print(t);
+  Serial.println("Humidity");
+  Serial.print(h);
+  String humid = "Humidity: " + String(h) + " % \n";
+  String temp = "Temperature: " + String(t) + "°C, " + String(f) + "°F \n";
+
+  
+  if (query == "temperature") {
+   
+    return temp;
+  }
+
+  else if (query == "humidity") {
+  
+    return humid;
+  }
+ 
+  else return "Your query was invalid..";
+
+}
+
+
+void loop()
+{ 
+
+  thing.Handle();
+
+}
